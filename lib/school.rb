7a8dd40ce45f66b39fc7a4a6651ba5c2f0e32a36
as30098 <alexspencer421@gmail.com>
2020-11-22 18:18:23 -0500

    commit tree ab61f5b4ef30561f6b9effd7880fe2ca9893fa0b

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+  end
+
+  def roster
+    hash = {}
+  end
+
+  def add_student(name, grade)
+    hash[name] = []
+    hash[name] << grade
+  end
+
+  def grade
+  end
+
+end
